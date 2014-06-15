998501a51d79614a6b4f33d62899befdd1aacf95
vagrant <vagrant@ubuntu-12.04-omnibus-chef>
2014-06-15 17:47:07 +0000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Popcorn::Application.config.secret_key_base = 'a0dd80d6cd06a8a2e0e9efc08681a9981d60bbf88502c324c9d8650614414393ad6b99b247cf4ca8750e5ed675f226387b118ab88120df84fda402b5a3c60f1e'
